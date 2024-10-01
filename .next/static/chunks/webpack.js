5248c09ad90c257fe66040311a88424fb3e593a6
Adophilus <uchenna19of@gmail.com>
2024-10-01 22:49:07 +0100

    removed clashing api index route file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cbb398d278b8430f"; }
+/******/ 		__webpack_require__.h = function() { return "7aa93309e6ea365a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
