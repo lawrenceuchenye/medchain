2b34a8d525871863eca8535d6be0105c930e0e1d
Lawrence <lawuche29@gmail.com>
2024-10-05 10:58:29 +0100

    YOLO

--- app/lib/ipfs.ts
+++ app/lib/ipfs.ts
@@ -6,14 +6,14 @@ export namespace IPFSService {
   export type UploadError = "UPLOAD_FAILED";
 
   export const uploadUserDetails = async (
-    userDetails: MedChainUser,
+    userDetails: MedChainUser
   ): Promise<Result<string, UploadError>> => {
     try {
       const res = await BackendService.client.api.user.upload.$post({
         json: userDetails,
       });
       const json = await res.json();
-      if (json.variant === "error") return Result.err("UPLOAD_FAILED");
+      if (json?.variant === "error") return Result.err("UPLOAD_FAILED");
       return Result.ok(json.data.hash);
     } catch (err) {
       return Result.err("UPLOAD_FAILED");
